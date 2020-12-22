a4086a889e8f93dd1aa22be3a837a118eed8181c
Matheus Kildere <matheuskildery94@gmail.com>
2020-12-21 22:44:54 -0300

    feat: double linked list

--- com/src/main/java/app/App.java
+++ com/src/main/java/app/App.java
@@ -1,5 +1,8 @@
 package app;
 
+import app.estudante.Estudante;
+import app.linkedList.DoubleLinkedList;
+
 /**
  * Hello world!
  */
@@ -12,6 +15,24 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        DoubleLinkedList linkedList = new DoubleLinkedList();
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 100000; i++) {
+            Estudante student = new Estudante();
+            linkedList.put(student.getMatricula(), student);
+        }
+        long endTime = System.nanoTime();
+        
+        long startTimeShow = System.nanoTime();
+        System.out.println(linkedList.toString());
+        System.out.println("Tempo de inicio: "+startTime);
+        System.out.println("Tempo ao finalizar: "+endTime);
+        System.out.println("Subtração: " + (endTime - startTime));
+        System.out.println("Tempo em segundos: " + (endTime - startTime) / 1_000_000_000f);
+        long endTimeShow = System.nanoTime();
+        System.out.println("\nTempo show: "+ startTimeShow);
+        System.out.println("Tempo ao finalizar show: "+endTimeShow);
+        System.out.println("Subtração: " + (endTimeShow - startTimeShow));
+        System.out.println("Tempo em segundos: " + (endTimeShow - startTimeShow) / 1_000_000_000f);
     }
 }
